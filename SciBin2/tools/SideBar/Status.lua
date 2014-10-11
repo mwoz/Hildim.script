66e97c0228bf1f8d461a7601d9bf1b542b7320d2
mwoz <mwoz@4b7732e4-2124-4528-be85-0b758191300a>
2014-10-11 17:13:52 +0000

    Диалог выбора цветов + удаление мусора

--- SciBin2/tools/SideBar/Status.lua
+++ SciBin2/tools/SideBar/Status.lua
@@ -1,6 +1,98 @@
 local txtCol, txtSel, lblSel, txtLine
 local isColor = false
 local needCoding = false
+
+local function ColorDlg()
+    local t = editor:GetText()
+    local tc={};tc['0 0 0']=true;tc['255 255 255']=true
+    for r,g,b in t:gmatch('"#?(%x%x)(%x%x)(%x%x)"') do
+        tc[((('0x'..r)+0)..' '..(('0x'..g)+0)..' ' ..(('0x'..b)+0))] = true
+    end
+    for r,g,b in t:gmatch('RGB%( *&H(%x%x)[, ]+&H(%x%x)[, ]+&H(%x%x)') do
+        tc[((('0x'..r)+0)..' '..(('0x'..g)+0)..' ' ..(('0x'..b)+0))] = true
+    end
+    for r,g,b in t:gmatch('RGB%( *([0-9]+)[, ]+([0-9]+)[, ]+([0-9]+)') do
+       tc[r..' '..g..' '..b] = true
+    end
+    local tc2={}
+    for c, _ in pairs(tc) do
+        table.insert(tc2,c)
+    end
+    local txtSample, clp, clb,txtOutB,txtOutF,txtT,txtG,txtB
+    local nTip,nCel, cB,cF = -1,0
+    local function StaticControls()
+        local prim,sec,r,g,b = iup.GetAttributeId(clp,'CELL',cF),iup.GetAttributeId(clp,'CELL',cB)
+        txtSample.bgcolor = sec
+        txtSample.fgcolor = prim
+        local _,_,r,g,b = prim:find('([0-9]+) ([0-9]+) ([0-9]+)')
+        txtOutF.value = string.format('RGB(%i, %i, %i)    #%02x%02x%02x',r,g,b,r,g,b)
+        _,_,r,g,b = sec:find('([0-9]+) ([0-9]+) ([0-9]+)')
+        txtOutB.value = string.format('RGB(%i, %i, %i)    #%02x%02x%02x',r,g,b,r,g,b)
+        local c = Iif(nTip==-1,prim,sec)
+    end
+    local function ByTxt()
+        local rgb=((('0x'..txtR.Value)+0)..' '..(('0x'..txtG.Value)+0)..' ' ..(('0x'..txtB.Value)+0))
+        clb.rgb= rgb
+        iup.SetAttributeId(clp,'CELL', nCel,rgb )
+        StaticControls()
+    end
+
+    clb=iup.colorbrowser{}
+
+    txtSample = iup.text{readonly='YES',value='some text some text some text ';expand='HORIZONTAL';maxsize=clb.rastersize;}
+    txtOutF = iup.text{readonly='YES';expand='HORIZONTAL'}
+    txtOutB = iup.text{readonly='YES';expand='HORIZONTAL'}
+    txtR = iup.text{mask='[abcdef0-9][abcdef0-9]?';expand='NO';size='40x0';action=ByTxt}
+    txtG = iup.text{mask='[abcdef0-9][abcdef0-9]?';expand='NO';size='40x0';action=ByTxt}
+    txtB = iup.text{mask='[abcdef0-9][abcdef0-9]?';expand='NO';size='40x0';action=ByTxt}
+
+    clp=iup.colorbar{num_parts=math.floor((#tc2)^0.5);num_cells=#tc2;show_secondary='YES';expand='NO';rastersize='260x200';
+        select_cb = (function(h,cell,tp)
+            local rgb = iup.GetAttributeId(h,'CELL',cell), tp
+            nTip=tp
+            nCel=cell
+            if tp == -1 then cF = cell else cB = cell end
+            clb.rgb= rgb
+            local _,_,r,g,b = rgb:find('([0-9]+) ([0-9]+) ([0-9]+)')
+            txtR.value = string.format('%02x',r)
+            txtG.value = string.format('%02x',g)
+            txtB.value = string.format('%02x',b)
+            StaticControls()
+        end)
+    }
+
+    for i, c in pairs(tc2) do
+        iup.SetAttributeId(clp,'CELL', i-1, c)
+    end
+    clb.drag_cb=(function(h,r,g,b)
+        iup.SetAttributeId(clp,'CELL', nCel,r..' '..g..' '..b )
+        txtR.value = string.format('%02x',r)
+        txtG.value = string.format('%02x',g)
+        txtB.value = string.format('%02x',b)
+        StaticControls()
+    end)
+
+    local dlg = iup.scitedialog{
+        iup.hbox{
+          iup.vbox{
+            clb;
+            iup.hbox{txtR;txtG;txtB};
+            txtSample;txtOutF;txtOutB,
+          expand='NO'};
+        clp};
+        title="Colors",defaultenter="MOVE_BTN_OK",defaultesc="MOVE_BTN_ESC",tabsize=editor.TabWidth,
+        maxbox="NO",minbox ="NO",shrink ="YES",sciteparent="SCITE", sciteid="color",resize='NO',
+        map_cb=(function() cB,cF=clp.secondary_cell,clp.primary_cell; StaticControls()end)}
+
+    dlg.show_cb=(function(h,state)                             -- "#e0e0e0"  "999999"
+        if state == 4 then
+            dlg:postdestroy()
+        end
+    end)
+    print()
+end
+
+
 local function ShowCurrentColour(pos, word)
 	if pos ~= 0 then
 		if word:match('%x%x%x%x%x%x') then
@@ -47,7 +139,22 @@ local function FindTab_Init()
     txtCol = iup.text{size='25x'; readonly='YES', bgcolor=iup.GetGlobal('DLGBGCOLOR')}
     txtSel = iup.text{size='25x'; readonly='YES', bgcolor=iup.GetGlobal('DLGBGCOLOR')}
     txtLine = iup.text{size='25x'; readonly='YES', bgcolor=iup.GetGlobal('DLGBGCOLOR')}
-    lblSel = iup.text{size = '200x0'; readonly='YES', bgcolor=iup.GetGlobal('DLGBGCOLOR')}
+    lblSel = iup.text{size = '200x0'; readonly='YES', bgcolor=iup.GetGlobal('DLGBGCOLOR'),
+        tip='Число вхождений выделенного слова'..Iif(editor.Lexer == SCLEX_FORMENJINE,'\nПоказ цвета под курсором', ''),
+        tips_cb=(function(h,x,y)
+            h.tip='Число вхождений выделенного слова'..Iif(editor.Lexer == SCLEX_FORMENJINE,'\nПоказ цвета под курсором', '')
+        end);
+        button_cb = (function(h, but, pressed, x, y, status)
+            if iup.isdouble(status) and iup.isbutton1(status) then
+                ColorDlg()
+            elseif iup.isbutton3(status) then
+                local mnu = iup.menu
+                {
+                  iup.item{title="Открыть палитру",action=ColorDlg}
+                }:popup(iup.MOUSEPOS,iup.MOUSEPOS)
+              return -1
+            end
+        end)}
     local function onSpellContext(_,but, pressed, x, y, status)
         if but == 51 and pressed == 0 then --right
 
