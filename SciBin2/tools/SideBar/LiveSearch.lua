9b2ba1bdab32c194109252a0262273c285037b08
mw <mw@4b7732e4-2124-4528-be85-0b758191300a>
2015-09-13 21:16:58 +0000

    очистка от лишних строк в поиске и консоли

--- SciBin2/tools/SideBar/LiveSearch.lua
+++ SciBin2/tools/SideBar/LiveSearch.lua
@@ -17,59 +17,42 @@ function sidebar_Find()
     txt_search.selection = '1:100'
 end
 local needCoding = false
-local function Find_onKey(key)
-    if key == 40 then  --down
-        local line = findrez:LineFromPosition(findrez.CurrentPos) + 1
-        findrez:SetSel(findrez:PositionFromLine(line), findrez.LineEndPosition[line])
-    elseif key == 38 then
-        local line = findrez:LineFromPosition(findrez.CurrentPos) - 1
-        findrez:SetSel(findrez:PositionFromLine(line), findrez.LineEndPosition[line])
-
-    elseif key == 13 then  --enter
-        local _,_, n = string.find(findrez:GetSelText(), ':(%d+):')
-        if n==nil then
-            Find_onChange()
-            return
-        end
-        editor:SetSel(editor:PositionFromLine(n-1), editor.LineEndPosition[n-1])
-        iup.PassFocus()
-    elseif key == 27 then
-        iup.PassFocus()
-    end
-end
-
-local function Find_onFocus(setfocus)
-    if not setfocus then
-        local a = findrez:findtext('^</\\', SCFIND_REGEXP, 0)
-        if a then
-            findrez.TargetStart = a
-            findrez.TargetEnd = a+3
-            findrez:ReplaceTarget('<')
-        end
-    end
-end
 
 local function OnSwitch()
     needCoding = (scite.SendEditor(SCI_GETCODEPAGE) ~= 0)
 end
 
 local function FindTab_Init()
-
+    local tm = iup.timer{time=300}
+    local tmConsole = iup.timer{time=1800000}
+    tmConsole.action_cb = (function()
+        if output.LineCount > 300 then
+            output.TargetStart = 0
+            output.TargetEnd = output:PositionFromLine(output.LineCount-300)
+            output:ReplaceTarget('')
+        end
+    end)
+    tmConsole.run="YES"
     txt_search = iup.text{expand='YES', tip='"Живой" поиск(Alt+F)\nСтрелки "вверх"/"вниз" - перемещение по списку результаов\nEnter - переход к найденному\nEsc - вернуться'}
-    local function Find_onChange(c)
-        btn_search.active = Iif(#c.value == 0, 'NO', 'YES')
+    local function Find_onTimer()
+        tm.run="NO"
         local a = findrez:findtext('^</\\', SCFIND_REGEXP, 0)
         if a then
             findrez.TargetStart = 0
             findrez.TargetEnd = findrez.LineEndPosition[findrez:LineFromPosition(a)]+1
             findrez:ReplaceTarget('')
         end
-        local str = c.value
+        local str = txt_search.value
         if tonumber(props["editor.unicode.mode"]) ~= IDM_ENCODING_DEFAULT then str = str:to_utf8(1251) end
         findSettings.findWhat = str
         findSettings:FindAll(50,true)
     end
-
+    tm.action_cb = (Find_onTimer)
+    local function Find_onChange()
+        btn_search.active = Iif(#txt_search.value == 0, 'NO', 'YES')
+        tm.run="NO"
+        tm.run="YES"
+    end
 
     txt_search.valuechanged_cb = (Find_onChange)
     txt_search.killfocus_cb = (function(h)
@@ -78,6 +61,13 @@ local function FindTab_Init()
             findrez.TargetStart = a
             findrez.TargetEnd = a+3
             findrez:ReplaceTarget('<')
+        end
+    end)
+    txt_search.getfocus_cb = (function(h)
+        if findrez.LineCount > 1000 then
+            findrez.TargetStart = findrez:PositionFromLine(1000)
+            findrez.TargetEnd = findrez.Length
+            findrez:ReplaceTarget('')
         end
     end)
     txt_search.k_any = (function(c, key)
@@ -89,7 +79,8 @@ local function FindTab_Init()
             findrez:SetSel(findrez:PositionFromLine(line), findrez.LineEndPosition[line])
 
         elseif key == 13 then  --enter
-            local _,_, n = string.find(findrez:GetSelText(), ':(%d+):')
+            local _,_, n = string.find(findrez:GetSelText(), '^%s*(%d+):')
+            if n==nil then n = string.find(findrez:GetSelText(), ':(%d+):') end
             if n==nil then
                 Find_onChange(c)
                 return
