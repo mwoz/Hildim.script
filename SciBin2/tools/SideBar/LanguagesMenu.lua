09ad52f73c586e3ea148576778b8b249738e61eb
mw <mw@4b7732e4-2124-4528-be85-0b758191300a>
2016-04-16 11:35:49 +0000

    доработки по фолдингу и скобкам

--- SciBin2/tools/SideBar/LanguagesMenu.lua
+++ SciBin2/tools/SideBar/LanguagesMenu.lua
@@ -1,8 +1,8 @@
 return {
-{"Open html.properties",  action ="scite.Open'..\\\\..\\\\languages\\\\html.properties'"},
-{"Open formenjine.properties",  action ="scite.Open'..\\\\..\\\\languages\\\\formenjine.properties'"},
-{"Open sql.properties",  action ="scite.Open'..\\\\..\\\\languages\\\\sql.properties'"},
-{"Open others.properties",  action ="scite.Open'..\\\\..\\\\languages\\\\others.properties'"},
-{"Open lua.properties",  action ="scite.Open'..\\\\..\\\\languages\\\\lua.properties'"},
-{"Open cpp.properties",  action ="scite.Open'..\\\\..\\\\languages\\\\cpp.properties'"},
+{"Open html.properties",  action ="scite.Open(props['SciteDefaultHome']..'\\\\languages\\\\html.properties')"},
+{"Open formenjine.properties",  action ="scite.Open(props['SciteDefaultHome']..'\\\\languages\\\\formenjine.properties')"},
+{"Open sql.properties",  action ="scite.Open(props['SciteDefaultHome']..'\\\\languages\\\\sql.properties')"},
+{"Open others.properties",  action ="scite.Open(props['SciteDefaultHome']..'\\\\languages\\\\others.properties')"},
+{"Open lua.properties",  action ="scite.Open(props['SciteDefaultHome']..'\\\\languages\\\\lua.properties')"},
+{"Open cpp.properties",  action ="scite.Open(props['SciteDefaultHome']..'\\\\languages\\\\cpp.properties')"},
 }
